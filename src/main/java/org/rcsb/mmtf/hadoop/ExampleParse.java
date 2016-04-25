49828eebd5b2436dcf918e7e3ee80e05dcf0f865
Anthony Bradley <anthony.richard.bradley@gmail.com>
2016-04-25 10:06:26 -0700

    Updates to the example script

--- src/main/java/org/rcsb/mmtf/hadoop/ExampleParse.java
+++ src/main/java/org/rcsb/mmtf/hadoop/ExampleParse.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.biojava.nbio.structure.Structure;
 import org.rcsb.mmtf.api.StructureDataInterface;
@@ -39,14 +40,14 @@ public class ExampleParse  implements Serializable {
 	public static void main(String[] args ) throws IOException
 	{
 		// The input path for the data.
-		String inPath = "/Users/anthony/mmtf-update/full";
+		String inPath = "/Users/anthony/full";
 		long startTime = System.currentTimeMillis();
 		// This is the default 2 line structure for Spark applications
 		SparkConf conf = new SparkConf().setMaster("local[*]")
 				.setAppName(ExampleParse.class.getSimpleName());
 		// Set the config for the spark context
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaPairRDD<String,Structure> jprdd = sc
+		JavaRDD<String> jprdd = sc
 				.sequenceFile(inPath, Text.class, BytesWritable.class, 8)
 				// Roughly thirty seconds
 				.mapToPair(t -> new Tuple2<String, byte[]>(t._1.toString(), ReaderUtils.deflateGzip(t._2.getBytes())))
@@ -55,10 +56,12 @@ public class ExampleParse  implements Serializable {
 				// Roughly a minute
 				.mapToPair(t -> new Tuple2<String, StructureDataInterface>(t._1,  new DefaultDecoder(t._2)))
 				// Roughly ten minutes to then parse in biojava
-				.mapToPair(new StructDataInterfaceToStructureMapper());
+				.mapToPair(new StructDataInterfaceToStructureMapper())
+				// Now map them into one list
+				.map(t -> t._1);
 
 		// Now print the number of sturctures parsed
-		System.out.println(jprdd.values().collect().size()+" structures parsed.");
+		System.out.println(jprdd.count()+" structures parsed.");
 		long endTime = System.currentTimeMillis();
 		System.out.println(endTime-startTime);
 		// Now move the folder 
